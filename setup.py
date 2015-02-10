b41e483951ea8d5bf9326243fbe0f2fc74780bb4
Rikanishu <adantess@gmail.com>
2015-02-11 02:45:05 +0300

    0.1.3 release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='static-bundle',
-    version='0.1.2',
+    version='0.1.3',
     packages=['static_bundle'],
     author='Rikanishu',
     author_email='rikanishu@gmail.com',
